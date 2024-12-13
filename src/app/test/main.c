d8ebbc531fc68c082695ef3f806a9250e65b3955
xuyangw123 <1050721793@qq.com>
2024-12-13 14:47:54 +0800

    第三次提交

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -5,10 +5,73 @@
 #include "net.h"
 #include "netif_pcap.h"
 #include "dbg.h"
+#include "nlist.h"
 
 net_err_t netdev_init(){
 	netif_pcap_open();
 	return NET_ERR_OK;
+}
+
+typedef struct _tnode_t
+{
+	nlist_node_t node;
+	int id;
+}tnode_t;
+
+void nlist_test(void){
+
+	#define NODE_CNT 	4	
+	tnode_t node[NODE_CNT];
+	nlist_t list;
+
+	nlist_init(&list);
+	for(int i = 0; i < NODE_CNT; i++){
+		node[i].id = i;
+		nlist_insert_first(&list, &node[i].node);
+	}
+
+
+	plat_printf("insert first\n");
+	nlist_node_t *p;
+	nlist_for_each(p, &list){
+		tnode_t * tnode = nlist_entry(p, tnode_t, node);
+		plat_printf("id: %d\n", tnode->id);
+	}
+	plat_printf("remove first\n");
+	for(int i = 0; i < NODE_CNT; i++){
+		p = nlist_remove_first(&list);
+		plat_printf("id:%d\n", nlist_entry(p, tnode_t, node)->id);
+	}
+
+
+	plat_printf("insert last\n");
+	for(int i = 0; i < NODE_CNT; i++){
+		nlist_insert_last(&list, &node[i].node);
+	}
+	nlist_for_each(p, &list){
+		tnode_t * tnode = nlist_entry(p, tnode_t, node);
+		plat_printf("id: %d\n", tnode->id);
+	}
+
+	plat_printf("remove last\n");
+	for(int i = 0; i < NODE_CNT; i++){
+		p = nlist_remove_last(&list);
+		plat_printf("id:%d\n", nlist_entry(p, tnode_t, node)->id);
+	}
+
+	plat_printf("insert after\n");
+	for(int i = 0; i < NODE_CNT; i++){
+		nlist_insert_after(&list, nlist_first(&list), &node[i].node);
+	}
+
+	nlist_for_each(p, &list){
+		tnode_t * tnode = nlist_entry(p, tnode_t, node);
+		plat_printf("id: %d\n", tnode->id);
+	}
+}
+
+void basic_test(void){
+	nlist_test();
 }
 
 
@@ -18,10 +81,11 @@ int main(void){
 	dbg_warning(DBG_TEST, "warning");
 	dbg_error(DBG_TEST, "error");
 	dbg_assert(1 == 1, "failed");
-	dbg_assert(1 == 0, "failed");
+
 
 	net_init();
 
+	basic_test();
 	net_start();
 
 	netdev_init();
